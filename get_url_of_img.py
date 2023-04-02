43cdf463cd1c5dc18551b91ec84ea23ffd02667c
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-04-02 08:28:18 +0200

    clean up script

--- get_url_of_img.py
+++ get_url_of_img.py
@@ -6,18 +6,11 @@ url = 'https://www.arthur-conan-doyle.com/index.php/The_Story_of_Spedegue%27s_Dr
 response = requests.get(url)
 
 soup = BeautifulSoup(response.text, "html.parser")  
-lst_img = []
-lst_img_p315 = []
-for item in soup.find_all('img'):
-	lst_img.append(item['src'])
-	if re.search('p315', item['src']):
-	    lst_img_p315.append(item['src'])
-	#print(item['src'])
+lst_links = []
+lst_links_jpg = []
+for link in soup.find_all('a'):
+	lst_links.append(link.get('href'))
+	#if '.jpg' in link.get('href'):
+	#	lst_links_jpg.append(link.get('href'))
 
-#print (item)
-#print(lst_img[0:10])
-
-#shutil.copyfileobj(res.raw, f)
-
-#https://www.arthur-conan-doyle.com/images/thumb/b/bd/The-strand-magazine-1928-10-the-story-of-spedegue-s-dropper-p315.jpg/624px-The-strand-magazine-1928-10-the-story-of-spedegue-s-dropper-p315.jpg
-#https://www.arthur-conan-doyle.com/images/thumb/4/4d/The-strand-magazine-1928-10-the-story-of-spedegue-s-dropper-p317.jpg/375px-The-strand-magazine-1928-10-the-story-of-spedegue-s-dropper-p317.jpg
+print(len(lst_links_jpg))
