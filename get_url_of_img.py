2c228e7f6af531d619e054c61595a7c38d4ed1cd
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-05-31 07:27:22 +0200

    update script

--- get_url_of_img.py
+++ get_url_of_img.py
@@ -1,12 +1,15 @@
+#%% libs
 import requests
 from bs4 import BeautifulSoup
 import re
 
+#%% specify url and path
 url_base = 'https://www.arthur-conan-doyle.com/'
 url_story = 'index.php/The_Story_of_Spedegue%27s_Dropper'
 url = url_base + url_story
+path = "./imgs/"
 
-# get all the links to the images (i.e. files ending in .jpg) from story's main website
+#%% get all the links to the images (i.e. files ending in .jpg) from story's main website
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")  
 lst_links = []
@@ -18,7 +21,7 @@ for link in soup.find_all('a'):
 			if link.get('href') not in lst_links_jpg:
 				lst_links_jpg.append(url_base + link.get('href'))
 
-# the links obtained above end in .jpg but do not actually lead to the images
+#%% the links obtained above end in .jpg but do not actually lead to the images
 # this requires another iteration, now filtering on the word images in the url
 lst_links_jpg_2 = []
 for link in lst_links_jpg:
@@ -32,12 +35,16 @@ for link in lst_links_jpg:
 					if url_base+item.get('href') not in lst_links_jpg_2:
 						lst_links_jpg_2.append(url_base + item.get('href'))
 
-# remove thumbnail images, i.e. those that contain the folder /thumb/ in their url
-print(len(lst_links_jpg_2))
+# %% remove thumbnail images, i.e. those that contain the folder /thumb/ in their url
 lst_links_jpg_3 = []
 for i in lst_links_jpg_2:
 	if re.search(r'/thumb/', i):
 		continue
 	lst_links_jpg_3.append(i)
-print(len(lst_links_jpg_3))
-print(lst_links_jpg_3)
+
+#%% download one of the images from the list
+import urllib.request
+url_tmp = lst_links_jpg_3[0]
+filename = path + url_tmp[-8:]
+urllib.request.urlretrieve(url_tmp, filename)
+# %%
